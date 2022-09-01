f9ec4f25bd7b564175aed113e11152058032f7ba
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-09-01 18:54:10 +0100

    enable prisma module globally

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
-import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
   controllers: [CustomersController],
   providers: [CustomersService],
-  imports: [PrismaModule],
 })
 export class CustomersModule {}
--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
-import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
   controllers: [EmployeesController],
   providers: [EmployeesService],
-  imports: [PrismaModule],
 })
 export class EmployeesModule {}
